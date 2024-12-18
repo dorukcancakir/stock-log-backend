ebdc4750d0792e5b6d9885b1093aa9e1035d04a2
Dorukcan ÇAKIR <mdorukcancakir99@gmail.com>
2024-12-18 14:33:02 +0300

    Update settings.py

--- stock_log/settings.py
+++ stock_log/settings.py
@@ -34,8 +34,13 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'https://stock-log.backendstocklog.work.gd',
     'https://*.backendstocklog.work.gd',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
 
 # Application definition
 
@@ -53,9 +58,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
